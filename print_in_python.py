9df3c2f9fbb300aecb61ae82781c5fb86c4b040a
sangam92 <sangam92@users.noreply.github.com>
2017-11-28 23:06:44 +0530

    Add files via upload

---
+++ print_in_python.py
@@ -0,0 +1,23 @@
+"""
+How to print in Python!!!
+
+it is very easy , we can write like a normal english .
+
+"""
+
+#Let type simple english
+
+print("Python Tutorials")
+
+#output - Python Tutorials
+
+#Let have some numbers!!
+
+print(2 + 3)
+
+#output - 5
+
+print("2 + 3")
+
+#output - 2 + 3
+
