9795e47780b0a9d729eb2e5c30ce7a18869a5479
sangam92 <sangam92@users.noreply.github.com>
2017-12-03 22:39:44 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,27 @@
+#operators in python
+
+
+#objects are the core things in python. 
+
+#there are 2 kinds of objects :- Scalar and Non Scalar.
+
+#Scalar objects are indivisble whila Non Scalar has their own internal structure.
+
+#Python has 4 main types of scalar objects.
+
+# int -----can be written like -3, 4 200045.
+
+print(4)
+
+#output ---4
+
+print(-3)
+
+#output --> -3
+
+# float is used for real numbers. we can also represent float using scientific notation.
+
+print(2e4)
+
+#output ---> 20000.0
+
