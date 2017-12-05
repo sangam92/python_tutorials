f997fdba6e1a77f6a28d99b8948ffef7a3c93272
sangam92 <sangam92@users.noreply.github.com>
2017-12-05 22:04:19 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,62 @@
+# variables is a way by which we associates names with objects.
+
+# A variable is associated with the objects with the help of assignment operator.
+
+#example:-
+
+a=4
+b=5
+
+print(a)
+
+print(b)
+
+
+# output --- 4 , 5
+
+#python variables are case sensitive.
+
+# python has a small number of keywords.
+
+#variables can contain uppercase,lowercase ,digit and special character.
+
+>>> a23 =45
+>>> print(a23)
+45
+
+#variables names cannot start with digit.
+
+>>> 2as =34
+  File "<stdin>", line 1
+    2as =34
+      ^
+SyntaxError: invalid syntax
+
+
+
+#python allows multiple assignment .
+
+a , b = 2, 3
+
+print (a,b)
+
+
+#output --- 2 3
+
+
+#another example :-
+
+a, b = 2,3
+
+a,b = b,a 
+
+
+print (a ,b)
+
+
+
+#output :-  3 2
+
+
+
+
