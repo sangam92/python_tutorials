1c6f0735a046f13155396cdaeee38d8958d26c33
sangam92 <sangam92@users.noreply.github.com>
2017-12-07 20:58:04 +0530

    Add files via upload

---
+++ branching.py
@@ -0,0 +1,31 @@
+#Branching Programs   
+
+# A branching condition has three parts :-
+
+# 1.) Test -----an expression that evaluates True and False. 
+
+# 2.) Block Code ------- A line of statement that defines the sequence of the code.
+
+# 3.) optional block of code --------in case the above block of code is not executed , this set of code will run.
+
+
+#Indentation---> it is  
+
+
+# few examples to illustrate :
+
+# For loop :-
+
+
+
+a = int(input())
+
+
+for i in range(a):
+    if i % 2 == 0 and i > 1:
+        print(i,"is an even number")
+    
+	
+#Python uses indentation unlike other programming languages which uses some sort of brackets like (),{},[].
+
+# In python indenation is a way to write a block of code and it marks the completion of the code.
