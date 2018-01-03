c1aaedbe2b6669b08cc5617fffd73731b106186f
sangam92 <sangam92@users.noreply.github.com>
2018-01-03 22:09:06 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+dict1={} #empty dictionary
+
+dict2={1:"test",2:"rest"} #dictionary with integer keys
+
+dict3={1:"test","name":"rest"} #dictionary with mixed type
+
+dict4= dict({1:"test",2:"rest"}) # dictionary with dict built in function
+
+dict5 = dict([(1,'apple'), (2,'ball')]) #from sequence having each item as a pair
+
