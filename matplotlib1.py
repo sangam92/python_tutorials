588b59e76d08a8b7c2f39d8b459da02688e3288d
Sangam Choubey <sangam92@users.noreply.github.com>
2019-01-04 07:06:52 +0530

    Add files via upload

---
+++ matplotlib1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 28 23:22:00 2018
+
+@author: sangam
+"""
+#importing the matplot library 
+import matplotlib.pyplot as pt
+
+#Creating the data 
+x =  [2,4,6,8,10]
+y =  [1,2,3,4,5]
+
+
+#plotting the data on x and Y axis
+pt.plot(x,y,label='linear')
+
+#Displaying the graph
+pt.show()
